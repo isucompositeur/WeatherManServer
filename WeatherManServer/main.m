aac11752b73929c58338902bbb09e8fca58830fc
Nicholas Meyer <nick@Carl.local>
2012-02-07 21:30:34 -0600

    Initial Commit

---
+++ WeatherManServer/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  WeatherManServer
+//
+//  Created by Nicholas Meyer on 2/7/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main (int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
